eb899d96b252cce62e9c3b4eeb45e8f374ba6086
William Fagan <will@wichitacode.com>
2015-10-08 10:56:33 -0700

    Bump version for release.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-CDN',
-    version='1.3.0',
+    version='1.4.0',
     url='https://github.com/wichitacode/flask-cdn',
     license='MIT',
     author='William Fagan',
